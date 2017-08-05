13aa798e49732c8a8ee976bac0e1be70e1da4e10
ndineshanbu <30342601+ndineshanbu@users.noreply.github.com>
2017-08-05 16:19:59 +0530

    Update g3.c

--- g3.c
+++ g3.c
@@ -1,1 +1,10 @@
-#
+#include<stdio.h>
+int main()
+{
+char c;
+scanf("%c",&c);
+if((c=='a')||(c=='e')||(c=='i')||(c=='o')||(c=='u')||(c=='A')||(c=='E')||(c=='I')||(c=='O')||(c=='U'))
+printf("vowel");
+  else
+    printf("consonant");
+}
